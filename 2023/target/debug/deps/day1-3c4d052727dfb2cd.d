1f0ec096c623df29800d954ce3901139dbd9381d
Rafael Morales <rafaelmoralesvenegas@gmail.com>
2024-11-30 17:27:37 -0300

    Didn't mean to add target dirs

--- 2023/target/debug/deps/day1-3c4d052727dfb2cd.d
+++
@@ -1,5 +0,0 @@
-/home/rafael/proyectos/advent_of_code_2023/2023/target/debug/deps/day1-3c4d052727dfb2cd: day1/src/lib.rs
-
-/home/rafael/proyectos/advent_of_code_2023/2023/target/debug/deps/day1-3c4d052727dfb2cd.d: day1/src/lib.rs
-
-day1/src/lib.rs:
