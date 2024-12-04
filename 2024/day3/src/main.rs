59f86fa4c4cf468ee06fa3f5f32ee5d715512d57
Rafael Morales <rafaelmoralesvenegas@gmail.com>
2024-12-03 22:02:37 -0300

    Day 3 - Puzzle two
    Ohh I really like this answer

--- 2024/day3/src/main.rs
+++ 2024/day3/src/main.rs
@@ -13,53 +13,59 @@ enum InstructionType {
     Mult(usize, usize),
 }
 
+fn get_numbers(input: &mut String) -> Option<usize> {
+    let prefix = input
+        .chars()
+        .take_while(|c| c.is_ascii_digit())
+        .collect::<String>();
+
+    if !prefix.is_empty() && prefix.len() <= 3 {
+        let num = prefix.parse().ok();
+        *input = input.strip_prefix(&prefix).unwrap().to_string();
+
+        num
+    } else {
+        None
+    }
+}
+
+fn get_mul(input: &mut String) -> Option<InstructionType> {
+    let left = get_numbers(input)?;
+    *input = input.strip_prefix(",")?.to_string();
+    let right = get_numbers(input)?;
+    *input = input.strip_prefix(")")?.to_string();
+
+    Some(InstructionType::Mult(left, right))
+}
+
 impl AoC for Day3 {
     fn parse(mut input: String) -> Self {
-        let mut input = input.as_mut_str();
         let mut instructions = Vec::new();
 
-        while let Some(index) = input.find("mul(") {
-            input = &mut input[(index + 4)..];
-
-            let num = input
-                .chars()
-                .take_while(|c| c.is_ascii_digit())
-                .collect::<String>();
-            input = &mut input[num.len()..];
-
-            let left = if !num.is_empty() && num.len() <= 3 {
-                num.parse::<usize>().unwrap()
-            } else {
-                continue;
-            };
-
-            let comma = input.chars().next();
-            input = &mut input[1..];
-
-            if comma != Some(',') {
+        while !input.is_empty() {
+            if let Some(next) = input.strip_prefix("do()") {
+                input = next.to_string();
+                instructions.push(InstructionType::Do);
                 continue;
             }
 
-            let num = input
-                .chars()
-                .take_while(|c| c.is_ascii_digit())
-                .collect::<String>();
-            input = &mut input[num.len()..];
-
-            let right = if !num.is_empty() && num.len() <= 3 {
-                num.parse::<usize>().unwrap()
-            } else {
-                continue;
-            };
-
-            let right_paren = input.chars().next();
-            input = &mut input[1..];
-
-            if right_paren != Some(')') {
+            if let Some(next) = input.strip_prefix("don't()") {
+                input = next.to_string();
+                instructions.push(InstructionType::Dont);
                 continue;
             }
 
-            instructions.push(InstructionType::Mult(left, right));
+            if let Some(next) = input.strip_prefix("mul(") {
+                input = next.to_string();
+
+                if let Some(mul) = get_mul(&mut input) {
+                    instructions.push(mul);
+                }
+
+                continue;
+            }
+
+            input.remove(0);
         }
 
         Self { instructions }
@@ -77,7 +83,21 @@ impl AoC for Day3 {
     }
 
     fn puzzle_two(&self) -> u64 {
-        todo!()
+        let mut doing_work = true;
+        let mut total = 0;
+        for instruction in self.instructions.iter() {
+            match instruction {
+                InstructionType::Do => doing_work = true,
+                InstructionType::Dont => doing_work = false,
+                InstructionType::Mult(l, r) => {
+                    if doing_work {
+                        total += (l * r) as u64;
+                    }
+                }
+            }
+        }
+
+        total
     }
 }
 
