0c2554ffb48badca3197c5c6c4db369c80f17393
Rafael Morales <rafaelmoralesvenegas@gmail.com>
2025-03-31 15:50:44 -0300

    Day 10 base
    olvide hacer el dia 9 parte dos oops

--- 2024/day10/src/main.rs
+++ 2024/day10/src/main.rs
@@ -1,3 +1,27 @@
+use aoc::AoC;
+use std::fs;
+
+struct Day10 {}
+
+impl AoC for Day10 {
+    fn parse(_input: String) -> Self {
+        todo!()
+    }
+
+    fn puzzle_one(&mut self) -> u64 {
+        todo!()
+    }
+
+    fn puzzle_two(&mut self) -> u64 {
+        todo!()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").expect("Input file is present and intact");
+    let mut day10 = Day10::parse(input);
+
+    print!("\n\tpuzzle one: > {}\n", day10.puzzle_one());
+
+    print!("\n\tpuzzle two: > {}\n", day10.puzzle_two());
 }
